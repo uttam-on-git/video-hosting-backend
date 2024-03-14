3dbcb88ad7e94483f017cb0d1e3cdb3d87d2ee94
Romendra Uttam <120070407+uttam-on-git@users.noreply.github.com>
2024-03-14 17:16:20 +0530

    add utility of Api error and response

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+const asyncHandler = (requestHnadler)=> {
+    (req, res, next) => {
+        Promise.resolve(requestHnadler(req, res, next)).
+        catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
