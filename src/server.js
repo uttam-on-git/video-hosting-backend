3278123c7e1c2ffed416e84b4c4d2f2b44406d45
Romendra Uttam <120070407+uttam-on-git@users.noreply.github.com>
2024-03-14 15:45:10 +0530

    connect to database

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv'
+
+import connectDB from './db/db.js'
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+// import express from 'express'
+// const app = express()
+
+
+// ( async ()=> {
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on('error', (error) => {
+//         console.log('ERR: ', error);
+//         throw error
+//        }
+//        )
+//        app.listen(process.env.MONGODB_URI, () => {
+//         console.log(`App is listening on port ${process.env.PORT}`)
+//        })
+//     } catch (error) {
+//         console.log('ERROR', error)
+//         throw err
+//     }
+// })()
