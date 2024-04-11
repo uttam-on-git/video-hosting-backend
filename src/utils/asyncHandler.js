034808c5ea20e2b3704d4117719ca39a3241f456
Romendra Uttam <120070407+uttam-on-git@users.noreply.github.com>
2024-04-11 10:47:50 +0530

    setup user-controller and user-router

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
-const asyncHandler = (requestHnadler)=> {
-    (req, res, next) => {
-        Promise.resolve(requestHnadler(req, res, next)).
+const asyncHandler = (requestHandler)=> {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
         catch((err) => next(err))
     }
 }
